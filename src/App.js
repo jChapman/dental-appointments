e175e7f075535976d2d445bca7efe2da2e590c8d
Jordan <jchapman134569@gmail.com>
2019-10-22 00:17:52 -0400

    Start of the view for the scheduler, picked out a calendar viewer library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
 import './App.css';
+
+const localizer = momentLocalizer(moment)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" height="500px">
+      <p>Dental Scheduler</p>
+      <Calendar localizer={localizer} events={[]} defaultView={Views.WEEK} views={[Views.WEEK, Views.MONTH, Views.DAY]} />
     </div>
   );
 }
